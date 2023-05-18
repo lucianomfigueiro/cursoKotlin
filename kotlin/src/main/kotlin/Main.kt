169c3e792e13b2103742eef0f92d2b440f55431d
lucianomfigueiro <luciano.teste37@gmail.com>
2023-05-18 08:54:24 -0300

    Primeiras aulas Kotlin: Introdução e variaveis

---
+++ kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Olá, Mundo!")
+
+    val nome: String = "Mel"
+    val sobrenome: String = "Bixitos"
+
+    var idade = 30
+    idade += 20
+
+    println("Olá, $nome $sobrenome")
+    println("idade = $idade")
+}
